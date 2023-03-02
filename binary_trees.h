450b0fa33c2a1ec5b7b12ab3b5484fc0fc4a4d96
JI-Maina <irungujmaina@gmail.com>
2023-03-02 00:02:19 -0800

    Leaves

--- binary_trees.h
+++ binary_trees.h
@@ -32,4 +32,9 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
+
+
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
+
 #endif /* BINARY_TREES_H */
