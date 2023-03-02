8e9982743719f922d641181bc2c64b5295c54a26
JI-Maina <irungujmaina@gmail.com>
2023-03-02 08:49:41 -0800

    Height

--- binary_trees.h
+++ binary_trees.h
@@ -31,10 +31,13 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-
+size_t binary_tree_height(const binary_tree_t *tree);
 
 
 size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
 
+
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
